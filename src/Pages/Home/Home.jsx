bbeb4a12234cf90db6c36471bddaa38ee91ea41e
unknown <amitbiswas9282@gmail.com>
2024-05-10 14:06:47 +0600

    Harmony Suites Complete

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,15 +1,15 @@
 // import Amenities from "../../Conponents/Amenities/Amenities";
 // import HarmonyDifference from "../../Conponents/HarmonyDifference/HarmonyDifference";
-// import HarmonySuites from "../../Conponents/HarmonySuites/HarmonySuites";
+import HarmonySuites from "../../Conponents/HarmonySuites/HarmonySuites";
 import Banner from "../Banner/Banner";
 
 const Home = () => {
   return (
     <div>
       <Banner></Banner>
-      {/* <HarmonySuites></HarmonySuites>
-      <Amenities></Amenities>
-      <HarmonyDifference></HarmonyDifference> */}
+      <HarmonySuites></HarmonySuites>
+      {/* <Amenities></Amenities>
+      <HarmonyDifference></HarmonyDifference>  */}
     </div>
   );
 };
