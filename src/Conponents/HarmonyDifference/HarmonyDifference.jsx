50ce017d6e52267540a9b75e766a0bc0b46bafe5
unknown <amitbiswas9282@gmail.com>
2024-05-13 13:00:01 +0600

    This is Harmony Difference ASO setup complete

--- src/Conponents/HarmonyDifference/HarmonyDifference.jsx
+++ src/Conponents/HarmonyDifference/HarmonyDifference.jsx
@@ -1,6 +1,17 @@
+import Aos from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
+
 const HarmonyDifference = () => {
+  useEffect(() => {
+    Aos.init();
+  }, []);
   return (
-    <div className="md:mt-20 mt-14 max-w-7xl mx-auto">
+    <div
+      className="md:mt-20 mt-14 max-w-7xl mx-auto"
+      data-aos="fade-up"
+      data-aos-anchor-placement="center-bottom"
+    >
       <h1 className="md:text-5xl text-3xl font-serif font-bold text-center md:mb-14 mb-10">
         THE HARMONY DIFFERENCE
       </h1>
