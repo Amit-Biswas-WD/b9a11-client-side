d59687747ad2d8bd6b699904fc74a383bbb75d1e
unknown <amitbiswas9282@gmail.com>
2024-05-13 23:20:01 +0600

    backend data send complete

--- src/Pages/RoomDetailsPage/RoomDetailsPage.jsx
+++ src/Pages/RoomDetailsPage/RoomDetailsPage.jsx
@@ -1,15 +1,15 @@
-import { useContext } from "react";
+// import { useContext } from "react";
 import { useLoaderData } from "react-router-dom";
-import { AuthContext } from "../../Conponents/Context/AuthContextProvider";
+// import { AuthContext } from "../../Conponents/Context/AuthContextProvider";
 import Aos from "aos";
 import "aos/dist/aos.css";
 import { useEffect } from "react";
 
 const RoomDetailsPage = () => {
+  // const { user } = useContext(AuthContext);
   const rooms = useLoaderData();
-  const { user } = useContext(AuthContext);
-
   const {
+    _id,
     room_description,
     price_per_night,
     room_size,
@@ -18,24 +18,40 @@ const RoomDetailsPage = () => {
     special_offers,
   } = rooms;
 
+  const handleRoomDetails = (event) => {
+    event.preventDefault();
+
+    const roomDetails = {
+      services_id: _id,
+      price: price_per_night,
+      size: room_size,
+      img: room_images,
+    };
+    console.log(roomDetails);
+
+    fetch("http://localhost:5000/booking", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(roomDetails),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   useEffect(() => {
     Aos.init();
   }, []);
 
-  fetch("http://localhost:5000/services", {
-    method: "POST",
-    headers: {
-      "content-type": "application/json",
-    },
-    body: JSON.stringify(),
-  })
-    .then((res) => res.json())
-    .then((data) => {
-      console.log(data);
-    });
-
   return (
-    <div
+    <form
+      onSubmit={handleRoomDetails}
       className="max-w-7xl mx-auto flex gap-10"
       data-aos="fade-up"
       data-aos-anchor-placement="center-bottom"
@@ -45,25 +61,29 @@ const RoomDetailsPage = () => {
       </div>
       <div className="w-1/2 flex-wrap h-full">
         <div>
-          <p className="text-lg font-normal my-2 ">
+          <p className="text-lg font-normal my-2">
             Price Par Night: {price_per_night}
           </p>
-          <p className="text-lg font-normal my-2 ">Room Size: {room_size}</p>
-          <p className="text-lg font-normal my-2 ">
+          <p className="text-lg font-normal my-2">Room Size: {room_size}</p>
+          <p className="text-lg font-normal my-2">
             Availability: {availability}
           </p>
-          <p className="text-lg font-normal my-2 ">
+          <p className="text-lg font-normal my-2">
             Special Offers: {special_offers}
           </p>
-          <p className="text-base font-normal my-2 ">
+          <p className="text-base font-normal my-2">
             Room Description: {room_description}
           </p>
         </div>
-        <div className="mt-auto">
-          <button className="btn btn-primary">Book Now</button>
+        <div className="form-control mt-6">
+          <input
+            className="btn btn-primary btn-block"
+            type="submit"
+            value="Order Confirm"
+          />
         </div>
       </div>
-    </div>
+    </form>
   );
 };
 
